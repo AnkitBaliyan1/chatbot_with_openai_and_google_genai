4390283c6533caf1350d58e7369a92987eaf62c7
ankit <a.baliyan008@gmail.com>
2023-12-26 16:53:00 +0530

    rearrange

--- chatbot/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-from django import forms
-
-class InputForm(forms.Form):
-    user_input = forms.CharField(
-        label='Prompt here..', 
-        max_length=100, 
-        widget=forms.TextInput(attrs={'class': 'large-input'})
-    )
